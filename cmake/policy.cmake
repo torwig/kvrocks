3372a6ffb0c44b9b58b665c6b94bfc89251f3046
Yaroslav Stepanchuk <torwigua@gmail.com>
2022-09-06 13:42:33 +0300

    Change formatting

--- cmake/policy.cmake
+++ cmake/policy.cmake
@@ -19,10 +19,8 @@
 # http://www.cmake.org/cmake/help/v3.0/command/cmake_policy.html
 
 macro (kvrocks_policy)
-
-	# Avoid warning about DOWNLOAD_EXTRACT_TIMESTAMP in CMake 3.24:
-	if (CMAKE_VERSION VERSION_GREATER_EQUAL "3.24.0")
-		cmake_policy(SET CMP0135 NEW)
-	endif()
-	
+  # Avoid warning about DOWNLOAD_EXTRACT_TIMESTAMP in CMake 3.24:
+  if (CMAKE_VERSION VERSION_GREATER_EQUAL "3.24.0")
+    cmake_policy(SET CMP0135 NEW)
+  endif()
 endmacro()
